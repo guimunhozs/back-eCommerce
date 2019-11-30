2176ba1c1faf214edd6c812d7796d2f5565c9cd3
guimunhozs <guilhermemunhozsilva@outlook.com>
2019-11-30 13:48:59 -0300

    config: adicionado gitignore e cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
